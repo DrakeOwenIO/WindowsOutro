5ae4603fdd021374a8bd6c410aa375ed3119449b
Drake Owen <[drake@reidfarms.com]>
2023-04-04 21:21:15 -0400

    another commmit

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <windows.h>
+#include <mmsystem.h>
+
+#pragma comment(lib, "winmm.lib")
+
+using namespace std;
+
+int playSong(){
+    // Start the song
+    PlaySound(TEXT("Outro.wav"), NULL, SND_FILENAME | SND_ASYNC);
+    return 0;
+}
+
+int main()
+{
+    // Declare dat boi
+    int countDown = 10;
+
+    // Outro Segment
+    cout<< "Thanks for using Windows!\n";
+    Sleep(1000);
+    cout<< "Shutdown in...\n";
+    Sleep(1000);
+
+    // Play the outro
+    playSong();
+
+    // Countdown loop
+    for (int i = countDown; i > 0; i--) {
+        cout << i << "\n";
+        Sleep(1000);
+    }
+    cout<<"\n";
+
+    system("c:\\windows\\system32\\shutdown /s /t 3");
+
+    cout << "Bye!";
+
+    Sleep(10000);
+    return 0;
+}
